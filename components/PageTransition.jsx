8c2731b3fc7499851461938c8bd4aa416441caf5
mohamed-alaa <muhammadalaa84@gmail.com>
2024-06-25 20:42:15 +0300

    adding the motion using framer motion

--- components/PageTransition.jsx
+++ components/PageTransition.jsx
@@ -1,10 +1,23 @@
 "use client";
 
-import { AnimatePresence } from "framer-motion"
+import { AnimatePresence, motion } from "framer-motion"
+import { usePathname } from "next/navigation";
 export default function PageTransition({children}) {
+    const pathname = usePathname();
+
     return(
         <AnimatePresence>
-            {children}
+            <div key={pathname}>
+                <motion.div 
+                initial={{opacity: 1}} 
+                animate={{
+                    opacity: 0,
+                    transition: {delay:.5, duration: 0.5, ease: "easeInOut"},
+                        }}
+                        className="h-screen w-screen fixed bg-primary top-0 pointer-events-none"
+                />
+                {children}
+            </div>
         </AnimatePresence>
     )
 }
---
+++ components/StairTransition.jsx
@@ -0,0 +1,22 @@
+"use client";
+import { AnimatePresence, easeIn, easeInOut, motion } from "framer-motion";
+import { usePathname } from "next/navigation";
+import Stairs from "./Stairs";
+export default function StairTransition() {
+    const pathname = usePathname();
+    return (
+        <AnimatePresence mode="wait">
+            <div key={pathname}>
+                <div 
+                className="h-screen w-screen fixed top-0 left-0 right-0 pointer-events-none z-40 flex">
+                <Stairs />
+                </div>
+                <motion.div 
+                className="h-screen w-screen fixed pg-primary top-0 pointer-events-none"
+                initial={{opacity:1}} animate={{opacity:0, transition:{delay: .5, duration:.4, ease: "easeInOut"}
+            }}
+                />
+            </div>
+        </AnimatePresence>
+    )
+}

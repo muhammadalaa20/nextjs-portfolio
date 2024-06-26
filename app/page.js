4231687fd4547caf13841b23b2fe69aead11e130
mohamed-alaa <muhammadalaa84@gmail.com>
2024-06-26 11:11:42 +0300

    editing the home page

--- app/page.js
+++ app/page.js
@@ -1,8 +1,29 @@
+import { Button } from "@/components/ui/button"
+import { FiDownload } from "react-icons/fi";
 
 export default function Home() {
   return (
     <main> 
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <section className="h-full">
+        <div className="container mx-auto border">
+          <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+            
+            <div className="text-center xl:text-left">
+              
+              <span>Electrical Engineer | Front-End Web Developer</span>
+              <h1>Hello I'm <br /> <span>Muhammad Alaa Mostafa</span> </h1>
+
+            </div>
+
+            <div>
+
+              photo
+              
+              </div>
+
+          </div>
+        </div>
+      </section>
     </main>
   )
 }
